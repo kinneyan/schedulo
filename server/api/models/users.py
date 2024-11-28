d5b8100ce2aa785c4de10a46084804a670179ad8
Andrew Kinney <ak@kinneyan.com>
2024-11-28 17:21:05 -0500

    Write user model

--- server/api/models/users.py
+++ server/api/models/users.py
@@ -1,2 +1,10 @@
 from django.db import models
 
+class User(models.Model):
+    date_created = models.DateTimeField(auto_now_add=True)
+    date_modified = models.DateTimeField(auto_now=True)
+    fname = models.CharField(max_length=30)
+    lname = models.CharField(max_length=30)
+    phone = models.CharField(max_length=30, null=True, blank=True)
+    email = models.EmailField(max_length=100)
+    password = models.CharField(max_length=100)
