64f4faf1e7b17b2c99c885528f0a90d370b31777
Andrew Kinney <ak@kinneyan.com>
2024-11-29 08:33:14 -0500

    Write shift request model

--- server/api/models/schedules.py
+++ server/api/models/schedules.py
@@ -12,3 +12,13 @@ class Shift(models.Model):
     role_id = models.ForeignKey(WorkspaceRole, on_delete=models.CASCADE)
     created_by_id = models.ForeignKey(WorkspaceMember, on_delete=models.CASCADE, related_name='created_shifts')
     open = models.BooleanField(default=False)
+
+class ShiftRequest(models.model):
+    date_created = models.DateTimeField(auto_now_add=True)
+    date_modified = models.DateTimeField(auto_now=True)
+    sender_id = models.ForeignKey(WorkspaceMember, on_delete=models.CASCADE)
+    recipient_id = models.ForeignKey(WorkspaceMember, on_delete=models.CASCADE)
+    sender_shift_id = models.ForeignKey(Shift, on_delete=models.CASCADE, related_name='shift_requests')
+    recipient_shift_id = models.ForeignKey(Shift, on_delete=models.CASCADE, null=True, blank=True)
+    accepted = models.BooleanField(default=False)
+    approved = models.BooleanField(default=False)
