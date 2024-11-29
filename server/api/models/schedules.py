a099a4b00b7a1658106f5e18c230802f0d5599a5
Andrew Kinney <ak@kinneyan.com>
2024-11-28 20:47:32 -0500

    Write shift model

--- server/api/models/schedules.py
+++ server/api/models/schedules.py
@@ -1,1 +1,14 @@
 from django.db import models
+from .users import WorkspaceMember, Workspace
+from .roles import WorkspaceRole
+
+class Shift(models.Model):
+    date_created = models.DateTimeField(auto_now_add=True)
+    date_modified = models.DateTimeField(auto_now=True)
+    member_id = models.ForeignKey(WorkspaceMember, on_delete=models.CASCADE)
+    workspace_id = models.ForeignKey(Workspace, on_delete=models.CASCADE)
+    start_time = models.DateTimeField()
+    end_time = models.DateTimeField()
+    role_id = models.ForeignKey(WorkspaceRole, on_delete=models.CASCADE)
+    created_by_id = models.ForeignKey(WorkspaceMember, on_delete=models.CASCADE, related_name='created_shifts')
+    open = models.BooleanField(default=False)
