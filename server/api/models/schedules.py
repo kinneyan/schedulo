b798b5218e8b700c850ec8e7b6824dce8ed03ded
Andrew Kinney <ak@kinneyan.com>
2024-11-29 08:58:09 -0500

    Fix migration process

--- server/api/models/schedules.py
+++ server/api/models/schedules.py
@@ -14,11 +14,11 @@ class Shift(models.Model):
     created_by_id = models.ForeignKey(WorkspaceMember, on_delete=models.CASCADE, related_name='created_shifts')
     open = models.BooleanField(default=False)
 
-class ShiftRequest(models.model):
+class ShiftRequest(models.Model):
     date_created = models.DateTimeField(auto_now_add=True)
     date_modified = models.DateTimeField(auto_now=True)
-    sender_id = models.ForeignKey(WorkspaceMember, on_delete=models.CASCADE)
-    recipient_id = models.ForeignKey(WorkspaceMember, on_delete=models.CASCADE)
+    sender_id = models.ForeignKey(WorkspaceMember, on_delete=models.CASCADE, related_name='sent_shift_requests')
+    recipient_id = models.ForeignKey(WorkspaceMember, on_delete=models.CASCADE, related_name='received_shift_requests')
     sender_shift_id = models.ForeignKey(Shift, on_delete=models.CASCADE, related_name='shift_requests')
     recipient_shift_id = models.ForeignKey(Shift, on_delete=models.CASCADE, null=True, blank=True)
     accepted = models.BooleanField(default=False)
@@ -35,7 +35,7 @@ class TimeOffRequest(models.Model):
     reason = models.TextField(blank=True)
     approved_by_id = models.ForeignKey(WorkspaceMember, on_delete=models.CASCADE, related_name='approved_time_off_requests', null=True, blank=True)
 
-class Unavailability(models.model):
+class Unavailability(models.Model):
     date_created = models.DateTimeField(auto_now_add=True)
     date_modified = models.DateTimeField(auto_now=True)
     member_id = models.ForeignKey(WorkspaceMember, on_delete=models.CASCADE)
@@ -44,7 +44,7 @@ class Unavailability(models.model):
     day_of_week = models.IntegerField(validators=[MinValueValidator(0), MaxValueValidator(6)])
 
     class Meta:
-        constrains = [
+        constraints = [
             models.CheckConstraint(
                 check=models.Q(day_of_week__gte=0) & models.Q(day_of_week__lte=6),
                 name="day_of_week_valid",
