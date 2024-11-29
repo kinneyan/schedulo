a4a9fc9b5aa81d5d39f12a194e0bf5636c0841b7
Andrew Kinney <ak@kinneyan.com>
2024-11-28 20:21:34 -0500

    Add role permissions

--- server/api/models/roles.py
+++ server/api/models/roles.py
@@ -7,3 +7,15 @@ class WorkspaceRole(models.Model):
     workspace_id = models.ForeignKey(Workspace, on_delete=models.CASCADE)
     name = models.CharField(max_length=30)
     pay_rate = models.DecimalField(max_digits=5, decimal_places=2, null=True, blank=True)
+
+class Permission(models.Model):
+    date_created = models.DateTimeField(auto_now_add=True)
+    date_modified = models.DateTimeField(auto_now=True)
+    name = models.CharField(max_length=30)
+    description = models.TextField(blank=True)
+
+class RolePermissions(models.Model):
+    date_created = models.DateTimeField(auto_now_add=True)
+    date_modified = models.DateTimeField(auto_now=True)
+    workspace_role_id = models.ForeignKey(WorkspaceRole, on_delete=models.CASCADE)
+    permission_id = models.ForeignKey(Permission, on_delete=models.CASCADE)
