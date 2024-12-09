e2e82c9acb6b56bbc84b3a133e02a04a38fbb852
Andrew Kinney <ak@kinneyan.com>
2024-12-09 10:33:54 -0500

    Write register endpoint

--- server/api/serializers/users.py
+++ server/api/serializers/users.py
@@ -15,3 +15,11 @@ class RegisterUserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
         fields = ['email', 'password', 'first_name', 'last_name', 'phone']
+
+        extra_kwargs = {
+            'email': {'required': True},
+            'password': {'required': True, 'write_only': True},
+            'first_name': {'required': True},
+            'last_name': {'required': True},
+            'phone': {'required': True},
+        }
--- server/api/views/register.py
+++ server/api/views/register.py
@@ -19,9 +19,23 @@ class Register(APIView):
             response["error"]["message"] = "Invalid request data"
             return Response(response, status=status.HTTP_400_BAD_REQUEST)
 
-        # try:
+        try:
+            user = User.objects.create_user(
+                email=serializer.validated_data["email"],
+                password=serializer.validated_data["password"],
+                first_name=serializer.validated_data["first_name"],
+                last_name=serializer.validated_data["last_name"],
+                phone=serializer.validated_data["phone"]
+            )
+            user.save()
 
-        # except: 
-        #     response["error"]["code"] = 400
-        #     response["error"]["message"] = "Invalid request data"
-        #     return Response(response, status=status.HTTP_400_BAD_REQUEST)
+            token_serializer = CustomTokenObtainPairSerializer(data={"email": user.email, "password": serializer.validated_data["password"]})
+            token_serializer.is_valid()
+            response["access"] = token_serializer.validated_data["access"]
+            response["refresh"] = token_serializer.validated_data["refresh"]
+            return Response(response, status=status.HTTP_201_CREATED)
+        
+        except Exception as e:
+            response["error"]["code"] = 500
+            response["error"]["message"] = "Internal server error"
+            return Response(response, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
