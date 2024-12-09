7444867fa3edaed25d717a6706585641028d5f8a
Andrew Kinney <ak@kinneyan.com>
2024-12-09 09:22:24 -0500

    Reverse test

--- server/api/tests/login.py
+++ server/api/tests/login.py
@@ -36,5 +36,5 @@ class LoginTests(APITestCase):
         }
         response = self.client.post(self.url, data, format='json')
         self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
-        self.assertEqual(response.data['error']['code'], 401)
+        self.assertEqual(response.data['error']['code'], 400)
         self.assertEqual(response.data['error']['message'], 'Invalid request data')
