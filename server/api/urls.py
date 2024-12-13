3bd48948b58fa22b7bf7d73cf3b9c03f2393d04f
Cteklitz <cteklitz@ufl.edu>
2024-12-13 15:42:08 -0500

    Write CreateWorkspace Endpoint

--- server/api/urls.py
+++ server/api/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 from rest_framework_simplejwt.views import TokenRefreshView
-from .views import Login, Register, GetUser
+from .views import Login, Register, GetUser, CreateWorkspace
 
 urlpatterns = [
     path('login', Login.as_view(), name='login'),
     path('register', Register.as_view(), name='register'),
     path('user', GetUser.as_view(), name='get_user'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('workspace/create/', CreateWorkspace.as_view(), name='create_workspace')
 ]
