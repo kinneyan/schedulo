92edf21f1d2cbd197c8ed017d965e6066e2b89ee
Andrew Kinney <ak@kinneyan.com>
2024-12-13 20:31:54 -0500

    Implement get permissions endpoint

--- server/api/urls.py
+++ server/api/urls.py
@@ -1,11 +1,12 @@
 from django.urls import path
 from rest_framework_simplejwt.views import TokenRefreshView
-from .views import Login, Register, GetUser, CreateWorkspace
+from .views import Login, Register, GetUser, CreateWorkspace, GetPermissions
 
 urlpatterns = [
     path('login', Login.as_view(), name='login'),
     path('register', Register.as_view(), name='register'),
     path('user', GetUser.as_view(), name='get_user'),
     path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    path('workspace/create/', CreateWorkspace.as_view(), name='create_workspace')
+    path('workspace/create/', CreateWorkspace.as_view(), name='create_workspace'),
+    path('workspace/permissions/', GetPermissions.as_view(), name='get_permissions')
 ]
--- server/api/views/__init__.py
+++ server/api/views/__init__.py
@@ -1,3 +1,4 @@
 from .auth import Login, Register
 from .user import GetUser
 from .workspace import CreateWorkspace
+from .permissions import GetPermissions
