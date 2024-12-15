07ee10a324ca78c556cc96538038332cec09bf4e
Cteklitz <cteklitz@ufl.edu>
2024-12-15 16:07:28 -0500

    Add modify workspace to URLs

--- server/api/views/__init__.py
+++ server/api/views/__init__.py
@@ -1,4 +1,4 @@
 from .auth import Login, Register
 from .user import GetUser
 from .permissions import GetPermissions, UpdatePermissions
-from .workspace import CreateWorkspace, AddWorkspaceMember
+from .workspace import CreateWorkspace, AddWorkspaceMember, ModifyWorkspace
