370f8ce7eca0672dbc8dbe8f53815996cf119a80
Cteklitz <cteklitz@ufl.edu>
2024-12-16 09:23:25 -0500

    Write Modify Workspace

--- server/api/views/workspace.py
+++ server/api/views/workspace.py
@@ -54,6 +54,87 @@ class CreateWorkspace(APIView):
 
         return Response(response, status=status.HTTP_201_CREATED)
     
+class ModifyWorkspace(APIView): # only change name and owner, can only be done by owner.
+    authentication_classes = [JWTAuthentication]
+    permission_classes = [IsAuthenticated]
+
+    def put(self, request):
+        response = {"error": {}}
+
+        serializer = WorkspaceSerializer(data=request.data)
+        if not serializer.is_valid():
+            response["error"]["code"] = 400
+            response["error"]["message"] = "Invalid request data"
+            return Response(response, status=status.HTTP_400_BAD_REQUEST)
+        
+        # Verify body contains required fields
+        if "workspace_id" not in request.data:
+            response["error"]["message"] = "Workspace ID is required."
+            return Response(response, status=status.HTTP_400_BAD_REQUEST)
+        
+        # Verify workspace exsists
+        try:
+            workspace = Workspace.objects.get(pk=request.data["workspace_id"])
+        except Workspace.DoesNotExist:
+            response["error"]["message"] = "Workspace does not exsist."
+            return Response(response, status=status.HTTP_404_NOT_FOUND)
+        
+        # Verify user is owner
+        try:
+            old_owner_member = WorkspaceMember.objects.get(user_id=request.user, workspace_id=request.data["workspace_id"])
+            permissions = MemberPermissions.objects.get(
+                workspace_id=request.data["workspace_id"],
+                member_id=old_owner_member,
+                IS_OWNER=True
+            )
+        except MemberPermissions.DoesNotExist:
+            response["error"]["message"] = "You do not have permission modify this workspace."
+            return Response(response, status=status.HTTP_403_FORBIDDEN)
+              
+      
+        # Update Workspace
+        if "new_owner_id" in request.data: # update workspace owner
+            # Check new owner
+            try:
+                new_owner_member = WorkspaceMember.objects.get(user_id=request.data["new_owner_id"], workspace_id=workspace)
+                new_owner_perms = MemberPermissions.objects.get(member_id=new_owner_member)
+                
+                # set current owner to no longer be owner
+                old_owner_perms = MemberPermissions.objects.get(member_id=old_owner_member, workspace_id=request.data["workspace_id"])
+
+                if (new_owner_perms == old_owner_perms): # cannot set new owner to current
+                    response["error"]["message"] = "Member is already owner of this workspace."
+                    return Response(response, status=status.HTTP_409_CONFLICT)
+
+                old_owner_perms.IS_OWNER = False
+                old_owner_perms.save()
+
+                # set new owner to be owner
+                new_owner_perms.IS_OWNER = True
+                new_owner_perms.MANAGE_WORKSPACE_MEMBERS = True
+                new_owner_perms.MANAGE_WORKSPACE_ROLES = True
+                new_owner_perms.MANAGE_SCHEDULES = True
+                new_owner_perms.MANAGE_TIME_OFF = True
+                new_owner_perms.save()
+
+                # update owner id in workspace
+                workspace.owner_id = User.objects.get(pk=request.data["new_owner_id"])
+                workspace.save()
+
+            except MemberPermissions.DoesNotExist:
+                response["error"]["message"] = "Could not find member with provided ID."
+                return Response(response, status=status.HTTP_404_NOT_FOUND)
+            except WorkspaceMember.DoesNotExist:
+                response["error"]["message"] = "Could not find member with provided ID."
+                return Response(response, status=status.HTTP_404_NOT_FOUND)
+
+        if not (serializer.data.get('name',None) == None): # update name
+            workspace.name = serializer.validated_data['name']
+            workspace.save()
+
+        return Response(response, status=status.HTTP_200_OK)
+
+    
 class AddWorkspaceMember(APIView):
     authentication_classes = [JWTAuthentication]
     permission_classes = [IsAuthenticated]
