da4437c9adae9ad6f00a2cfbd522f39a9c685369
Andrew Kinney <ak@kinneyan.com>
2024-12-16 19:55:55 -0500

    Write navbar component

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,12 @@
-import { useState } from 'react'
+import { useState } from 'react';
+import NavigationBar from './components/navbar';
 
 function App() {
   return (
     <>
+      <NavigationBar />
     </>
-  )
-}
+  );
+};
 
 export default App
---
+++ client/src/components/navbar/index.jsx
@@ -0,0 +1,28 @@
+import Container from 'react-bootstrap/Container';
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
+
+import './index.scss';
+
+const NavigationBar = () => {
+    return (
+        <Navbar fixed="top">
+            <Container fluid className="p-0">
+                <Navbar.Brand href="#">
+                    <img 
+                        src='/schedulo.png'
+                        alt='logo'
+                    />
+                    {' '}Schedulo
+                </Navbar.Brand>
+                <Nav id="page-links">
+                    <Nav.Link href="#">Home</Nav.Link>
+                    <Nav.Link href="#">About</Nav.Link>
+                    <Nav.Link href="#">Log in</Nav.Link>
+                </Nav>
+            </Container>
+        </Navbar>
+    );
+};
+
+export default NavigationBar;
