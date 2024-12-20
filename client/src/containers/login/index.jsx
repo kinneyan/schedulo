59680c3ea577e1b6cfb28a13b510ea56f5bf9d7f
Andrew Kinney <ak@kinneyan.com>
2024-12-20 14:41:52 -0500

    Save cookie on successful login

--- client/src/containers/login/index.jsx
+++ client/src/containers/login/index.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import LoginForm from '../../components/loginform';
+import Cookies from 'universal-cookie';
 
 const LoginContainer = () =>{
     const [email, setEmail] = useState('');
@@ -10,18 +11,30 @@ const LoginContainer = () =>{
         e.preventDefault(); // stop default form submission
 
         try {
-            const response = await fetch(import.meta.env.VITE_API_URL + '/api/login', { // Update the URL to your login endpoint
+            const response = fetch(import.meta.env.VITE_API_URL + '/api/login', { // Update the URL to your login endpoint
                 method: 'POST',
                 headers: { 'Content-Type': 'application/json' },
                 body: JSON.stringify({ email, password }),
+            }).then((response) => response.json()).then((response) => {
+
+                // save cookie
+                const body = {
+                    'access': response['access'],
+                    'refresh': response['refresh'],
+                }
+
+                const cookies = new Cookies();
+                cookies.set('token', JSON.stringify(body), {
+                    path: '/' , 
+                    sameSite: 'Lax',
+                    expires: new Date(Date.now() + 86400000), // Set to 1 day
+                });
+
+                // redirect
+
             });
 
-            if (!response.ok) {
-                throw new Error('Incorrect email or password');
-            }
 
-            // Handle successful login here (e.g., save token, redirect)
-            console.log("WORKED");
         } catch (error) {
             console.log("Error: ", error);
             setError("Incorrect email or password");
