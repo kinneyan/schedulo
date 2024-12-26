d8a8807fa60a7109e3b30c041ce4219726e1f545
Cteklitz <cteklitz@ufl.edu>
2024-12-26 13:42:28 -0500

    Write Shift serializer

---
+++ server/api/serializers/shift.py
@@ -0,0 +1,12 @@
+from rest_framework import serializers
+from ..models import Shift
+
+class ShiftSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Shift
+        fields = ['start_time', 'end_time']
+
+        extra_kwargs = {
+            'start_time': {'required': True},
+            'end_time': {'required': True},
+        }
