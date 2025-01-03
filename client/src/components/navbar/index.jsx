0f199fa73a7c626d06479a3e237f3d4b83204e57
Andrew Kinney <ak@kinneyan.com>
2025-01-03 13:29:05 -0500

    Check logged in state for navbar
    If logged in, change navbar button to "log out" and vice versa.

--- client/src/components/navbar/index.jsx
+++ client/src/components/navbar/index.jsx
@@ -1,10 +1,24 @@
 import Container from 'react-bootstrap/Container';
 import Navbar from 'react-bootstrap/Navbar';
 import Nav from 'react-bootstrap/Nav';
+import Cookies from 'universal-cookie';
 
 import './index.scss';
 
-const NavigationBar = () => {
+const log_out = () => {
+    const cookies = new Cookies();
+    cookies.remove('token');
+};
+
+const AuthButton = ({ logged_in }) => {
+    if (!logged_in)
+    {
+        return <Nav.Link href="/login">Log in</Nav.Link>;
+    }
+    return <Nav.Link href="/" onClick={log_out()} >Log out</Nav.Link>;
+};
+
+const NavigationBar = ({ logged_in }) => {
     return (
         <Navbar fixed="top">
             <Container fluid className="p-0">
@@ -18,7 +32,7 @@ const NavigationBar = () => {
                 <Nav id="page-links">
                     <Nav.Link href="/">Home</Nav.Link>
                     <Nav.Link href="#">About</Nav.Link>
-                    <Nav.Link href="/login">Log in</Nav.Link>
+                    <AuthButton logged_in={logged_in} />
                 </Nav>
             </Container>
         </Navbar>
