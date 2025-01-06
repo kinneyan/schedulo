0664b3ced58bece9492f466fed025f3473e58f94
Cteklitz <cteklitz@ufl.edu>
2025-01-06 14:58:50 -0500

    Uncomment tests
    Commented out the tests to speed up testing

--- server/api/tests/__init__.py
+++ server/api/tests/__init__.py
@@ -1,9 +1,6 @@
-'''
 from .auth import LoginTests, RegisterTests
 from .user import GetUserTests, UpdateUserTests
 from .permissions import UpdatePermissionsTests
 from .workspace import AddMemberTests, ModifyWorkspaceTests
 from .role import CreateRoleTests, GetRolesTests, AddMemberRoleTests, RemoveMemberRoleTests, DeleteRoleTests, GetMemberRoleTests, ModifyWorkspaceRoleTests
 from .shift import CreateShiftTests, ModifyShiftTests, DeleteShiftTests, GetShiftsTest
-'''
-from .shift import GetShiftsTest
