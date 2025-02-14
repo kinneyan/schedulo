0f0f8857c7c2c8baf535e86fa5dac1095f1d88d8
Andrew Kinney <ak@kinneyan.com>
2025-02-13 19:47:47 -0500

    Stash changes

--- client/src/pages/profile/index.jsx
+++ client/src/pages/profile/index.jsx
@@ -1,13 +1,15 @@
 import React from 'react';
 import './index.scss';
 
-import NavbarContainer from '../../containers/navbar';
 import with_auth from '../../components/auth';
+import NavbarContainer from '../../containers/navbar';
+import Profile from '../../containers/profile';
 
 const ProfilePage = () => {
     return (
         <div>
             <NavbarContainer />
+            <Profile />
         </div>
     );
 };
