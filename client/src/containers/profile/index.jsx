bacc23f0c7c1271b3bee5fa21eaedcd0b0b24f53
Andrew Kinney <ak@kinneyan.com>
2025-02-13 19:48:14 -0500

    Stash changes

---
+++ client/src/containers/profile/index.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+import ViewProfile from '../../components/profile';
+
+const Profile = () => {
+    return (
+        <div>
+            <ViewProfile />
+        </div>
+    );
+};
+
+export default Profile;
