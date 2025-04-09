8d56a2516bcac52c385ab61acd84b79c37e43d15
Andrew Kinney <ak@kinneyan.com>
2025-04-09 17:40:19 -0400

    Update user data

--- client/src/containers/profile/index.jsx
+++ client/src/containers/profile/index.jsx
@@ -5,6 +5,11 @@ import ViewProfile from '../../components/profile';
 
 const Profile = () => 
 {
+    // get token from cookies
+    const cookies = new Cookies();
+    const token = cookies.get("token");
+
+    // create states
     const [fname, setFname] = useState("");
     const [lname, setLname] = useState("");
     const [email, setEmail] = useState("");
@@ -33,16 +38,12 @@ const Profile = () =>
             // populate user information
             try 
             {
-                // get token from cookies
-                const cookies = new Cookies();
-                const token = cookies.get("token");
-        
                 const response = await fetch(import.meta.env.VITE_API_URL + "/api/user", 
                 {
                     method: "GET",
                     withCredentials: true,
-                    credentials: 'include',
-                    headers: { 'Content-Type': 'application/json', 'Authorization': 'Bearer ' + token.access},
+                    credentials: "include",
+                    headers: {"Content-Type": "application/json", "Authorization": "Bearer " + token.access},
                 });
         
                 const data = await response.json();
@@ -70,6 +71,45 @@ const Profile = () =>
     const handleSubmit = async (e) => 
     {
         e.preventDefault();     // stop default form submission
+
+        // build request body
+        let requestBody = {
+            first_name: fname,
+            last_name: lname,
+            email: email,
+            phone: phone,
+        }
+
+        if (newPassword !== "")
+        {
+            requestBody.old_password = oldPassword;
+            requestBody.new_password = newPassword;
+
+        }
+
+        // make request
+        try 
+        {
+            const response = await fetch(import.meta.env.VITE_API_URL + "/api/user", 
+            {
+                method: "PUT",
+                withCredentials: true,
+                credentials: "include",
+                headers: {"Content-Type": "application/json", "Authorization": "Bearer " + token.access},
+                body: JSON.stringify({requestBody}),
+            });
+
+            const data = await response.json();
+
+            if (response.status == 400)
+            {
+                throw new Error(data["error"]);
+            }
+        }
+        catch (error)
+        {
+            console.log(error);
+        }
 
     };
 
