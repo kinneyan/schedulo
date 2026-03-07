478d3e87e762ac6b41d744dacb86c4609748cf95
kinneyan <ak@kinneyan.com>
2026-03-06 22:58:36 -0500

    feat: configure whitenoise for static file serving

---
+++ server/api/tests/unit/test_settings.py
@@ -0,0 +1,28 @@
+from pathlib import Path
+from django.test import TestCase
+from django.conf import settings
+
+
+class WhiteNoiseSettingsTest(TestCase):
+    def test_whitenoise_in_middleware(self):
+        self.assertIn(
+            "whitenoise.middleware.WhiteNoiseMiddleware",
+            settings.MIDDLEWARE,
+        )
+
+    def test_whitenoise_after_security_middleware(self):
+        idx_security = settings.MIDDLEWARE.index(
+            "django.middleware.security.SecurityMiddleware"
+        )
+        idx_whitenoise = settings.MIDDLEWARE.index(
+            "whitenoise.middleware.WhiteNoiseMiddleware"
+        )
+        self.assertEqual(idx_whitenoise, idx_security + 1)
+
+    def test_static_root_configured(self):
+        self.assertIsInstance(settings.STATIC_ROOT, Path)
+        self.assertEqual(settings.STATIC_ROOT.name, "staticfiles")
+
+    def test_whitenoise_root_configured(self):
+        self.assertIsInstance(settings.WHITENOISE_ROOT, Path)
+        self.assertEqual(settings.WHITENOISE_ROOT.name, "static_build")
--- server/server/settings/default.py
+++ server/server/settings/default.py
@@ -47,6 +47,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
+    "whitenoise.middleware.WhiteNoiseMiddleware",
     "corsheaders.middleware.CorsMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
@@ -152,6 +153,8 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = "static/"
+STATIC_ROOT = BASE_DIR / "staticfiles"
+WHITENOISE_ROOT = BASE_DIR / "static_build"
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
