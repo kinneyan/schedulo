19de1b1d08745c0ab1b810902eaec371d2f84594
kinneyan <ak@kinneyan.com>
2026-03-09 19:14:43 -0400

    chore: fix python formatting findings

--- server/server/utils.py
+++ server/server/utils.py
@@ -15,5 +15,7 @@ def require_env(name: str) -> str:
     """
     value = os.environ.get(name)
     if not value:
-        raise ImproperlyConfigured(f"Required environment variable '{name}' is not set.")
+        raise ImproperlyConfigured(
+            f"Required environment variable '{name}' is not set."
+        )
     return value
