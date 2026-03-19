30a102dddaa88d53161660661ad3809aa9a71911
Cteklitz <cteklitz@ufl.edu>
2026-03-19 16:22:00 -0400

    feat: add permissions serializer

---
+++ server/api/serializers/permissions.py
@@ -0,0 +1,7 @@
+from rest_framework import serializers
+from ..models import MemberPermissions
+
+class PermissionsReadSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = MemberPermissions
+        fields = ["is_owner", "manage_workspace_members", "manage_workspace_roles", "manage_schedules", "manage_time_off"]
